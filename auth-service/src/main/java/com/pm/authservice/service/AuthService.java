ebc7363e450867eebbba6171542774acb17323f0
Shivay Sharma <sharmashivay37@gmail.com>
2025-07-19 03:38:23 +0100

    Added The login and validate methods in auth service

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -3,6 +3,8 @@ package com.pm.authservice.service;
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.dto.LoginResponseDTO;
 import com.pm.authservice.model.User;
+import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,12 @@ public class AuthService {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
@@ -24,9 +28,18 @@ public class AuthService {
                 .findByEmail(loginRequestDTO.getEmail())
                 .filter(u -> passwordEncoder
                         .matches(loginRequestDTO.getPassword(),u.getPassword()))
-                .map(u -> JwtUtil.generateToken(u.getEmail(),u.getRole()));
+                .map(u -> jwtUtil.generateToken(u.getEmail(),u.getRole()));
         return token;
     }
 
+    public boolean validateToken(String token) {
+        try{
+            jwtUtil.validateToken(token) ;
+            return true;
+        }
+        catch (JwtException e){
+            return false ;
+        }
+    }
 
 }
